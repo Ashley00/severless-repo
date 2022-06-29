15ca0bc4777b594a27ac93fd7d63f3554061ebf5
Ashley <57233192+Ashley00@users.noreply.github.com>
2022-06-29 15:58:27 -0600

    add nums

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,16 @@
 // start coding your function here!
+function find_max(arr)
+{
+    const max_num = arr.at(0);
+    for(let i = 0; i < arr.length; i++)
+    {
+        const current = arr.at(i);
+        if (current > max_num)
+        {
+            max_num = current;
+        }
+    }
+    return max_num;
+}
+
+exports.find_max = find_max;
